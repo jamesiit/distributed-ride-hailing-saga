f184c5a0940bd257c370092783fdd106becf8d5c
James Motha <james.20232826@iit.ac.lk>
2026-05-12 16:19:50 +0530

    Load Test k6 idempotency and Dispatch update
    - Simulated a load test using k6 to test on idempotency holding up. 50 VU's fired with the same idempotency key and tripId.
    
    - DispatchController updated back to logical path and flow.

--- backend/dispatch-service/src/main/java/com/example/dispatchservice/controller/DispatchController.java
+++ backend/dispatch-service/src/main/java/com/example/dispatchservice/controller/DispatchController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DispatchController {
 
-//    private final DispatchService dispatchService;
+    private final DispatchService dispatchService;
 
-//    public DispatchController(DispatchService dispatchService) {
-//        this.dispatchService = dispatchService;
-//    }
+    public DispatchController(DispatchService dispatchService) {
+        this.dispatchService = dispatchService;
+    }
 
     @GetMapping("/dispatch")
     public String sayHello() {
@@ -27,12 +27,9 @@ public class DispatchController {
     @PostMapping("/dispatch")
     public ResponseEntity<?> createDispatch(@RequestBody DispatchDTO dispatchDTO) {
         try {
-            // some valid logic
-            // Dispatch createDispatch = dispatchService.createDispatch(dispatchDTO);
-            // return new ResponseEntity<>(createDispatch.getDispatchId(), HttpStatus.CREATED);
 
-            // simulate a business logic exception where drivers aren't found
-            return new ResponseEntity<>("No driver found!", HttpStatus.NOT_FOUND);
+            Dispatch createDispatch = dispatchService.createDispatch(dispatchDTO);
+            return new ResponseEntity<>(createDispatch.getDispatchId(), HttpStatus.CREATED);
 
         } catch (Exception e) {
             return new ResponseEntity<>("Something went wrong!", HttpStatus.INTERNAL_SERVER_ERROR);
