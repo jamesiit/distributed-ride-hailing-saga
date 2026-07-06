1ce17e764a4b087616836975311ef0d63d19e8db
jamesiit <james.20232826@iit.ac.lk>
2026-07-06 19:22:57 +0530

    SagaVPC CDK Deployment
    - Create VpcStack class. Add builder methods on VPC.
    
    - Provision overall CIDR block for VPC, AZ, NGW, Public and Private subnets
    
    - Use stable CDK version as opposed to the provided range upon init in pom.xml

--- backend/cdk/src/test/java/com/myorg/CdkTest.java
+++ backend/cdk/src/test/java/com/myorg/CdkTest.java
@@ -9,7 +9,7 @@
 // import org.junit.jupiter.api.Test;
 
 // example test. To run these tests, uncomment this file, along with the
-// example resource in java/src/main/java/com/myorg/CdkStack.java
+// example resource in java/src/main/java/com/myorg/VpcStack.java
 // public class CdkTest {
 
 //     @Test
