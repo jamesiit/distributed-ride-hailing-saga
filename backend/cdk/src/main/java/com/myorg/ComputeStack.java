6ff1791f98e3b399c38e941b320d4c591819b1c0
jamesiit <168275449+jamesiit@users.noreply.github.com>
2026-07-13 23:19:18 +0530

    Create ComputeStack
    - Create ComputeStack which will hold logic for IAM roles and Docker provision.
    
    - Create sample Task Execution Role for Fargate to access ECR and pull down images to be loaded into containers

---
+++ backend/cdk/src/main/java/com/myorg/ComputeStack.java
@@ -0,0 +1,24 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
+import software.constructs.Construct;
+
+public class ComputeStack extends Stack {
+
+    public ComputeStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public ComputeStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // create a task execution role so that Fargate has permission to go to ECR, pull down the image and load it into a container
+        Role taskExecutionRole = Role.Builder.create(this, "SagaTaskExecutionRole")
+                .assumedBy(new ServicePrincipal("ecs-tasks.amazonaws.com"))
+                .build();
+
+    }
+}
