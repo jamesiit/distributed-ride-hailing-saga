99a00a5530b6f5a764e59bad85157e38dfe3e4cf
jamesiit <james.20232826@iit.ac.lk>
2026-07-23 10:20:16 +0530

    feat(trip): setup ecs task, secrets & cloudmap
    - Initialize IAM role which will be used by the services to access other AWS Services
    
    - Initialize shared cloudwatch logs driver for the services
    
    - Define TaskDefinition for trip service. Specify hardware capacity, attach IAM role, execution role
    
    - Define port mappings for trip container. Inject SERVER_PORT as 8080 value and container port mapped to 8080, hence solving port conflicts
    
    - Implement code that inject other secrets to container OS
    
    - Implement circuitBreaker() logic, CloudMap configurations for trip service
    
    - Made task execution role comment clearer

--- backend/cdk/src/main/java/com/myorg/ComputeStack.java
+++ backend/cdk/src/main/java/com/myorg/ComputeStack.java
@@ -31,7 +31,7 @@ public class ComputeStack extends Stack {
                 .name("saga.local")
                 .build());
 
-        // create a task execution role so that Fargate has permission to go to ECR, pull down the image and load it into a container
+        // create a task execution role so that Fargate will have permission to go to ECR, pull down the image and load it into a container
         Role taskExecutionRole = Role.Builder.create(this, "SagaTaskExecutionRole")
                 .assumedBy(new ServicePrincipal("ecs-tasks.amazonaws.com"))
                 .build();
@@ -179,5 +179,51 @@ public class ComputeStack extends Stack {
         ContainerImage dispatchServiceImage = ContainerImage.fromAsset(
                 new File("../dispatch-service").getAbsolutePath()
         );
+
+        // initialize the IAM role which the app tasks will use
+        Role appTaskRole = Role.Builder.create(this, "AppTaskRole")
+                .assumedBy(new ServicePrincipal("ecs-tasks.amazonaws.com"))
+                .build();
+
+        // initialize CloudWatch logs for saga apps
+        LogDriver appLogDriver = LogDrivers.awsLogs(AwsLogDriverProps.builder()
+                .streamPrefix("SagaApp")
+                .build());
+
+        // app 1 - trip service
+        FargateTaskDefinition tripAppTask = FargateTaskDefinition.Builder.create(this, "TripAppTask")
+                .memoryLimitMiB(512)
+                .cpu(256)
+                .executionRole(taskExecutionRole)
+                .taskRole(appTaskRole)
+                .build();
+
+        tripAppTask.addContainer("TripAppContainer", ContainerDefinitionOptions.builder()
+                .image(tripServiceImage)
+                .essential(true)
+                .portMappings(List.of(PortMapping.builder()
+                        .containerPort(8080)
+                        .build()))
+                .logging(appLogDriver)
+                .environment(Map.of(
+                        "SPRING_DATASOURCE_URL", "jdbc:mysql://trip-db.saga.local:3306/trip_service?allowPublicKeyRetrieval=true&useSSL=false",
+                        "SERVER_PORT", "8080"
+                ))
+                .secrets(Map.of(
+                        "SPRING_DATASOURCE_PASSWORD", Secret.fromSsmParameter(dbPassword),
+                        "SPRING_DATASOURCE_USERNAME", Secret.fromSsmParameter(dbUsername)
+                ))
+                .build());
+
+        FargateService.Builder.create(this, "TripAppService")
+                .cluster(ecsCluster)
+                .taskDefinition(tripAppTask)
+                .circuitBreaker(DeploymentCircuitBreaker.builder()
+                        .rollback(true)
+                        .build())
+                .cloudMapOptions(CloudMapOptions.builder()
+                        .name("trip-service")
+                        .build())
+                .build();
     }
 }
